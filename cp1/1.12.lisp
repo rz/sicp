25895ed7c0a2c64617ecfaf1028a207c909c7303
Rodrigo Guzman <rodguze@gmail.com>
2016-08-04 16:09:33 -0400

    get rid of old solutions to start over

--- cp1/1.12.lisp
+++
@@ -1,4 +0,0 @@
-(defun pascal(n k)
-  (cond ((= k 0) 1)
-	((<= n k) 1)
-	(t (+ (pascal (- n 1) (- k 1)) (pascal (- n 1) k)))))
--- cp1/1.3.lisp
+++
@@ -1,5 +0,0 @@
-;;in clisp
-(defun f(a b c) 
-  (cond ((and (< c b) (< c a)) (+ (* a a) (* b b)))
-	((and (< a b) (< a c)) (+ (* b b) (* c c)))
-	(t (+ (* a a) (* c c)))))
