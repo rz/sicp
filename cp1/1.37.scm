86e0b8ad4712a26d0da2da7b992459bc355fcdf6
Rodrigo Guzman <rodguze@gmail.com>
2016-10-31 07:23:04 -0400

    fix off-by-1 bug in cont-frac

--- cp1/1.37.scm
+++ cp1/1.37.scm
@@ -1,21 +1,20 @@
 (define (r-cont-frac n-term d-term k)
-  (if (= k 1)
+  (if (= k 0)
     (/ (n-term k) (d-term k))
     (/ (n-term k) (+ (d-term k) (r-cont-frac n-term d-term (- k 1))))))
 
 
-(r-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 10) ; .6179775280898876
-(r-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11) ; .6180555555555556
-
+(r-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 10) ; .6180555555555556
+(r-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11) ; .6180257510729613
 
 
 (define (i-cont-frac n-term d-term k)
   (define (iter i result)
-    (if (= i 1)
+    (if (= i 0)
       result
       (iter (- i 1) (/ (n-term i) (+ result (d-term i))))))
   (iter k 1))
 
 
-(i-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11) ; .6180555555555556
+(i-cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11) ; .6180257510729613
 
