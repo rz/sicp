8d7f0a9c2d76f1ce68a1fc0ee4c19959ed39d6a4
Rodrigo Guzman <rodguze@gmail.com>
2017-02-17 06:03:47 -0500

    exercises 2.24 and 2.25

---
+++ cp2/2.25.scm
@@ -0,0 +1,45 @@
+(define l1 (list 1 3 (list 5 7) 9))
+(define l2 (list (list 7)))
+(define l3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+;;; to check that i defined the right lists:
+(newline)
+(display l1) (newline)
+(display l2) (newline)
+(display l3) (newline)
+(newline)
+;;; outputs:
+;;; (1 3 (5 7) 9)
+;;; ((7))
+;;; (1 (2 (3 (4 (5 (6 7))))))
+
+(newline)
+(display (car (cdr (car (cdr (cdr l1))))))
+(newline)
+;;; outputs 7
+
+(newline)
+(display (car (car l2)))
+(newline)
+;;; outputs 7
+
+(newline)
+(display (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l3)))))))))))))
+(newline)
+;;; outputs 7
+
+;;; make the 3rd one a little bit prettier:
+
+(newline)
+(display (cadr (cadr (cadr (cadr (cadr (cadr l3)))))))
+(newline)
+;;; outputs 7
+
+
+;;; and shorter but less clear:
+
+(newline)
+(display (cadadr (cadadr (cadadr l3))))
+(newline)
+;;; outputs 7
+
