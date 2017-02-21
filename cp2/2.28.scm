2b9c6e330d5e05bdb234049bdad8337b956d6893
Rodrigo Guzman <rodguze@gmail.com>
2017-02-21 07:15:43 -0500

    exercise 2.28

---
+++ cp2/2.28.scm
@@ -0,0 +1,8 @@
+(load "cp2/2.18.scm")
+
+(define (fringe tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree)) (fringe (cdr tree))))))
+
+
