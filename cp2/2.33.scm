453e1ea3fe3f504ea0fb94f4ed7521eb5a74bce2
Rodrigo Guzman <rodguze@gmail.com>
2017-12-24 12:33:42 -0800

    add an implementation of filter to exercise 2.33

--- cp2/2.33.scm
+++ cp2/2.33.scm
@@ -7,6 +7,10 @@
 (define (map p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
 
+;; this one is just for fun: map and filter can be implemented in terms of accumulate/reduce! ie reduce is universal/complete in some sense
+(define (filter p? sequence)
+  (accumulate (lambda (x y) (if (p? x) (cons x y)  y)) '() sequence))
+
 (define (append seq1 seq2)
   (accumulate cons seq2 seq1))
 
@@ -14,5 +18,6 @@
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
 (map square '(1 2 3 4)) ; returns (1 4 9 16)
+(filter even? '(1 2 3 4 5 6 7 8))
 (append '(1 2 3) '(4 5 6)) ; returns (1 2 3 4 5 6)
 (length '(1 2 3 4)) ; returns 4
